c1d06bacdab382f4dcd0586bf01d564362a187d7
Wayne Otido <otidowayne@gmail.com>
2023-06-19 14:32:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){
+  customerName= customerName.toUpperCase()
+}
+
+var bestCustomer
+function setBestCustomer(){
+  bestCustomer  = 'not bob'
+}
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'still not bob'
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'bob'
+}
